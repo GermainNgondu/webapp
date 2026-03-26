a4c5b8303b93b6182aef8bd15d1b369d4636ca7a
dmtbsi <it@cetdmtb.org>
2026-03-26 15:48:46 +0100

    feat: changes folder and clean installer

--- app/Core/Installer/Domain/Data/InstallData.php
+++ app/Core/Installer/Domain/Data/InstallData.php
@@ -1,24 +1,12 @@
 <?php
 
-namespace App\Core\Installer\Data;
+namespace App\Core\Installer\Domain\Data;
 
-use App\Core\Framework\Support\DataForm\Attributes\{Field, Step,Repeater};
-use App\Core\Installer\Http\Actions\SaveProjectIdentity;
 use Spatie\LaravelData\Data;
 
 class InstallData extends Data
 {
     public function __construct(
-        #[Step(name: 'Société', icon: 'building', action: SaveProjectIdentity::class)]
-        #[Field(label: 'Nom de la société', required: true)]
-        public string $name,
 
-        #[Step(name: 'Contact', icon: 'user')]
-        #[Repeater(label: 'Contacts', dataClass: ContactData::class)]
-        public array $contacts = [],
-        
-        #[Step(name: 'Configuration', icon: 'settings')]
-        #[Field(label: 'Activer le SSL', type: 'toggle')]
-        public bool $ssl = true,
     ) {}
 }
